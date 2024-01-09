dcd400bb3bb7571de4cfb5054d0cae6bd15c29c2
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2024-01-09 12:32:13 -0500

    updated code.py

--- code.py
+++ code.py
@@ -4,5 +4,16 @@
 # Date: Jan *8, 2024
 # This programme displays a greeting for me
 
-print("Hello Val!!!")
-print("How's your day??")
+def gamescene():
+    # This function is the main game scene
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello Val!!!")
+    print("How's your day??")
+
+    while True:
+        # repeat forever, or you turn it off!
+        pass # Placeholder
+
+if __name__ == "__main__":
+    gamescene()
