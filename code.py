5efa2ac2b9ff2261c47d511608f657af22ef4aff
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2024-01-09 13:00:38 -0500

    updated code.py

--- code.py
+++ code.py
@@ -2,14 +2,20 @@
 
 # Created By: Val I
 # Date: Jan *8, 2024
-# This programme displays a greeting for me
+# Game file for cops simulator
+
+import ugame
+import stage
 
 def gamescene():
-    # This function is the main game scene
+    # This function is the main game scene.
 
-    print("\n\n\n") # 3 blank lines
-    print("Hello Val!!!")
-    print("How's your day")
+    image_bank_background =stage.Bank.from_bmp16("space_aliens_background")
+    background = stage.Grid(space_aliens_background, 10, 8)
+
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
 
     while True:
         # repeat forever, or you turn it off!
