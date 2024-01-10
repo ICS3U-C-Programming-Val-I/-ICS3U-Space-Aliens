b9c96246530140a7939c437f89e99fc5e498df99
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2024-01-10 11:45:01 -0500

    updated code.py

--- code.py
+++ code.py
@@ -10,16 +10,24 @@ import stage
 def gamescene():
     # This function is the main game scene.
 
-    image_bank_background =stage.Bank.from_bmp16("space_aliens_background")
-    background = stage.Grid(space_aliens_background, 10, 8)
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    character = stage.Sprite(image_bank_sprites, 5, 75, 66)
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
     game.render_block()
 
     while True:
-        # repeat forever, or you turn it off!
-        pass # Placeholder
+        # get user input
+
+        # update game logic
+        
+        # redraw Sprites
+        game.render_sprites([ship])
+        game.tick()
 
 if __name__ == "__main__":
     gamescene()
