0364314f494f1597d37fd015a5b3bac8abebac33
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2024-01-10 12:39:54 -0500

    updated code.py

--- code.py
+++ code.py
@@ -17,13 +17,13 @@ def gamescene():
 
     character = stage.Sprite(image_bank_sprites, 5, 75, 66)
     game = stage.Stage(ugame.display, 60)
-    game.layers = [ship] + [background]
+    game.layers = [character] + [background]
     game.render_block()
 
     while True:
         # get user input
         keys = ugame.buttons.get_pressed()
-        
+
         if keys & ugame.K_X:
             print("A")
         if keys & ugame.K_O:
@@ -33,17 +33,17 @@ def gamescene():
         if keys & ugame.K_SELECT:
             print("Select")
         if keys & ugame.K_RIGHT:
-            ship.move(ship.x + 1, ship.y)
+            character.move(character.x + 1, character.y)
         if keys & ugame.K_LEFT:
-            ship.move(ship.x 1, ship.y)
+            character.move(character.x 1, character.y)
         if keys & ugame.K_UP:
-            ship.move(ship.x, ship.y - 1)
+            character.move(character.x, character.y - 1)
         if keys & ugame.K_DOWN:
-            ship.move(ship.x, ship.y + 1)
+            character.move(character.x, character.y + 1)
         # update game logic
         
         # redraw Sprites
-        game.render_sprites([ship])
+        game.render_sprites([character])
         game.tick()
 
 if __name__ == "__main__":
