6eca495b3fb5a48b722f2c6321b25c9f0fe11fef
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2024-01-11 13:11:57 -0500

    updated code.py

--- code.py
+++ code.py
@@ -43,7 +43,7 @@ def gamescene():
         keys = ugame.buttons.get_pressed()
 
         # A button to fire
-        if keys & ugame.K_0 != 0:
+        if keys & ugame.K_O != 0:
             if a_button == constants.button_state["button_up"]:
                 a_button = constants.button_state["button_just_pressed"]
             elif a_button == constants.button_state["button_just_pressed"]:
