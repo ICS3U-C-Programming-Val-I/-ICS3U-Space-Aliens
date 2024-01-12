441164c1fbbb3014f0ebb4505f63ebe95cbf27a1
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2024-01-12 13:09:41 -0500

    updated code.py

--- code.py
+++ code.py
@@ -7,11 +7,69 @@
 import ugame
 import stage
 import constants
+import time
+import random
+
+def splash_scene():
+    # This function is the splash scene.
+
+     # get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+    
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    
+    background = stage.Grid(image_bank_mt_background, 10, 8)
+
+    # used this program to split the image into tile: 
+
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+    
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = [background]
+    game.render_block()
+
+    # repeat forever, game loop
+    while true:
+        # Wait for 2 seconds
+        time.sleep(2.0)
+        menu_scene()
 
 def menu_scene():
-    # This function is the main game scene.
+    # This function is the menu scene.
 
-    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
     
     # add text objects
     text = []
@@ -27,7 +85,7 @@ def menu_scene():
 
 
 
-    background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(image_bank_mt_background, 10, 8)
 
     game = stage.Stage(ugame.display, constants.FPS)
     game.layers = text + [background]
@@ -42,6 +100,7 @@ def menu_scene():
 
         # redraw Sprites
         game.tick()
+
 
 def gamescene():
     # This function is the main game scene.
